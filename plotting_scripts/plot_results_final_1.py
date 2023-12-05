15e5c6ac934d294f7bcc344bccc31dacf78a91fd
Nikita Lazarev <nikita.u.lazarev@gmail.com>
2023-12-05 14:05:10 -0500

    plotting scripts improved

--- plotting_scripts/plot_results_final_1.py
+++ plotting_scripts/plot_results_final_1.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Load the data from the CSV file
-file_path = 'Downloads/raw_data-vanilla.csv'  # Replace with your file path
+file_path = 'Downloads/raw_data-REAP.csv'  # Replace with your file path
 data = pd.read_csv(file_path, header=2)
 
 text_size_ultrabig = 26
@@ -34,9 +34,9 @@ benchark_names = {
 # Renaming columns for easier access
 data.columns = [
     'Application', 'MemSize_MB', 'VanilaSnapshotSize_MB', "WarmCompute_ms",
-    'VanilaVMLoad_us', 'VanilaColdStart_us', 'VanilaTotal_us',
-    'AcceleratedVMLoad_us', 'AcceleratedColdStart_us', 'AcceleratedTotal_us',
-    'SnapshotSize', 'CompressionRatio', 'SpeedupPercentage'
+    'VanilaVMLoad_us', 'VanilaRestore', 'VanilaColdStart_us', 'VanilaTotal_us',
+    'AcceleratedVMLoad_us', 'AcceleratedRestore', 'AcceleratedColdStart_us', 'AcceleratedTotal_us',
+    'SnapshotSize', 'CompressionRatio', 'SpeedupPercentage', 'SpeedupRestoration'
 ]
 
 # Dropping unnecessary columns
@@ -63,34 +63,36 @@ accelerated_colors = ['gray', 'darkred']  # Two shades for Accelerated
 width = 0.25
 
 # Plotting
-plt.figure(figsize=(24, 10))
+fig, ax = plt.subplots(figsize=(24, 10))
 for i, app in enumerate(data['Application']):
     # Vanila times
     vanila_total = data.loc[i, 'VanilaTotal_us']
-    plt.bar(i - width/1.8, data.loc[i, 'VanilaVMLoad_us'], width=width, color=vanila_colors[0], label='Baseline VM load with on-demand paging' if i == 0 else "", zorder=3)
-    plt.bar(i - width/1.8, data.loc[i, 'VanilaColdStart_us'], width=width, color=vanila_colors[1], bottom=data.loc[i, 'VanilaVMLoad_us'], 
+    ax.bar(i - width/1.8, data.loc[i, 'VanilaVMLoad_us'], width=width, color=vanila_colors[0], label='Baseline VM load with REAP prefetching' if i == 0 else "", zorder=3)
+    ax.bar(i - width/1.8, data.loc[i, 'VanilaColdStart_us'], width=width, color=vanila_colors[1], bottom=data.loc[i, 'VanilaVMLoad_us'], 
             label='Baseline function invocation' if i == 0 else "", zorder=3)
 
     # Accelerated times
     accelerated_total = data.loc[i, 'AcceleratedTotal_us']
-    plt.bar(i + width/1.8, data.loc[i, 'AcceleratedVMLoad_us'], width=width, color=accelerated_colors[0], label='"Our" VM load with prefetching' if i == 0 else "", zorder=3, hatch='o')
-    plt.bar(i + width/1.8, data.loc[i, 'AcceleratedColdStart_us'], width=width, color=accelerated_colors[1], bottom=data.loc[i, 'AcceleratedVMLoad_us'], 
-            label='"Our" function invocation' if i == 0 else "", zorder=3, hatch='o')
+    ax.bar(i + width/1.8, data.loc[i, 'AcceleratedVMLoad_us'], width=width, color=accelerated_colors[0], label='Our VM load with prefetching' if i == 0 else "", zorder=3, hatch='o')
+    ax.bar(i + width/1.8, data.loc[i, 'AcceleratedColdStart_us'], width=width, color=accelerated_colors[1], bottom=data.loc[i, 'AcceleratedVMLoad_us'], 
+            label='Our function invocation' if i == 0 else "", zorder=3, hatch='o')
 
     # Adding compression ratio annotations
     compression_ratio = data.loc[i, 'CompressionRatio']
     # Positioning the annotation above the highest bar
     highest_point = max(vanila_total, accelerated_total)
-    plt.text(i, highest_point, f'{compression_ratio:.2f}x', ha='center', va='bottom', zorder=4, fontsize=text_size_big, color='black')
+    ax.text(i, highest_point, f'{compression_ratio:.2f}x', ha='center', va='bottom', zorder=4, fontsize=text_size_big, color='black')
 
 b_names = [benchark_names[b] for b in data['Application']]
-plt.xticks(range(len(data['Application'])), b_names, rotation=45, fontsize=text_size_big)
-plt.ylabel('End-to-end cold start time, ms', fontsize=text_size_ultrabig)
-plt.yticks(range(0, 10000, 1500), fontsize=text_size_ultrabig)
-plt.gca().spines['top'].set_visible(False)
-plt.gca().spines['right'].set_visible(False)
-plt.gca().spines['bottom'].set_visible(False)
-plt.gca().spines['left'].set_visible(False)
+ax.set_xticks(range(len(data['Application'])), b_names, rotation=45, fontsize=text_size_big)
+ax.set_ylabel('End-to-end cold start time, ms', fontsize=text_size_ultrabig)
+ax.set_yticks(range(0, 6000, 1000))
+for label in ax.get_yticklabels():
+    label.set_fontsize(text_size_ultrabig)
+ax.spines['top'].set_visible(False)
+ax.spines['right'].set_visible(False)
+ax.spines['bottom'].set_visible(False)
+ax.spines['left'].set_visible(False)
 # plt.title('VM Boot and Cold Start Times for Applications (in milliseconds)')
 plt.grid(axis='y', zorder=0)
 plt.gca().set_axisbelow(True)
